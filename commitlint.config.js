021edfd5474772b59d9474d93edd38dd092b85ff
za-sunxiaoju <xiaoju.sun@zatech.com>
2021-04-11 12:38:51 +0800

    test(SCOPE): subject full.stop?

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,7 +5,7 @@ module.exports = {
     'type-case': [0], // 类型大小写
     'type-empty': [2, 'never'], // 枚举允许为空  0为允许为空
     'scope-empty': [0], // scope  允许为空
-    'scope-case': [0],  // scope 不校验
+    'scope-case': [2, 'never'],  // scope 不校验
     'subject-full-stop': [2, 'never'],
     // 'subject-case': [0, 'never'],
     // 'header-max-length': [0, 'always', 72]
