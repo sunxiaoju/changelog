0434834ebe5c06f6fd527613a8ef80b5d353b94e
za-sunxiaoju <xiaoju.sun@zatech.com>
2021-04-11 12:32:19 +0800

    : testrules

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,7 +3,7 @@ module.exports = {
   rules: {
     "type-enum": [2, 'always', ['fix', 'feat', 'docs', 'style', 'test', 'perf']], // type 枚举
     'type-case': [0], // 类型大小写
-    'type-empty': [2, 'always'], // 枚举允许为空不允许为空
+    'type-empty': [0, 'always'], // 枚举允许为空
     // 'scope-empty': [0],
     // 'scope-case': [0],
     // 'subject-full-stop': [0, 'never'],
