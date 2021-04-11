3c91046acdb5876155c708448786654289e232c1
za-sunxiaoju <xiaoju.sun@zatech.com>
2021-04-11 12:36:28 +0800

    test(commitlint): testrules

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,9 +3,9 @@ module.exports = {
   rules: {
     "type-enum": [2, 'always', ['fix', 'feat', 'docs', 'style', 'test', 'perf']], // type 枚举
     'type-case': [0], // 类型大小写
-    'type-empty': [0, 'always'], // 枚举允许为空
-    // 'scope-empty': [0],
-    // 'scope-case': [0],
+    'type-empty': [2, 'never'], // 枚举允许为空  0为允许为空
+    'scope-empty': [0], // scope  允许为空
+    'scope-case': [0],  // scope 不校验
     // 'subject-full-stop': [0, 'never'],
     // 'subject-case': [0, 'never'],
     // 'header-max-length': [0, 'always', 72]
